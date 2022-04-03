8edb722223141087875b52caf87d07a7728b1d84
ASUS <ASUS@LAPTOP-VI1QT254>
2022-04-03 21:16:23 +0530

    c

---
+++ sb_security_in_memory_thymeleaf/src/main/java/com/example/sb_security_in_memory/SbSecurityInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_security_in_memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityInMemoryApplication.class, args);
+	}
+
+}
---
+++ sb_security_in_memory_thymeleaf/src/test/java/com/example/sb_security_in_memory/SbSecurityInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_security_in_memory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
